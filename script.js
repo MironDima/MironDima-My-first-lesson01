278fa288f36c7de9bdf50edb9f13df857ede1bb0
«Dima_Mirontsov» <miron2098@mail.ru>
2023-01-27 15:41:12 +0300

    вывел в консль title,fullPrice,adaptive

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+//misson
+let title = "Второй урок";
+console.log(typeof title);
 
+let screens = "Простые, Сложные, Интерактивные"
+
+
+let screenPrice = 24;
+
+let rollback  = 17;
+
+let fullPrice = 100000;
+console.log(typeof fullPrice);
+
+let adaptive = true;
+console.log(typeof adaptive);
