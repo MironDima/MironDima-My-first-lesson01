45851fb9b296a9c4de0fcc3af26625b65237808b
«Dima_Mirontsov» <miron2098@mail.ru>
2023-01-27 13:50:58 +0300

    добавив title  в index.html, удалил содержиоме в script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const title ='title';
-const screens = 10;
-const screenPrice = 210;
-let rollBack = 'откат';
-let fullPrice;
-let adaptive;
- 
-alert ('My first lesson')
-
-console.log('Hello my freid');
 
