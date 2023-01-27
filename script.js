24b4a0b9361376e27b88cd4492f59057078d04a6
«Dima_Mirontsov» <miron2098@mail.ru>
2023-01-27 16:06:12 +0300

    вывел в консоль screenPrice and fullPrice

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 //misson
 let title = "Второй урок";
-console.log(typeof title);
+console.log(typeof title); //тип
 
 let screens = "Простые, Сложные, Интерактивные"
-console.log(screens.length);
+console.log(screens.length);   //длина
 
 
 let screenPrice = 24;
+console.log("Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани")  //предложение
 
-let rollback  = 17;
+let rollback  = 17;    
 
 let fullPrice = 100000;
-console.log(typeof fullPrice);
+console.log(typeof fullPrice);  //тип
+console.log("Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани") //предложение
 
-let adaptive = true;
-console.log(typeof adaptive);
+
+let adaptive = true;     
+console.log(typeof adaptive);  //тип
+
+
