d29e89076c9836d1ca3fef187322ab48e720f7c1
«Dima_Mirontsov» <miron2098@mail.ru>
2023-01-27 23:07:18 +0300

    обновилЭ

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
-//misson
-let title = "Второй урок";
-console.log(typeof title); //Вывести в консоль тип данных значений переменной title
+const title ='title';
+const screens = 10;
+const screenPrice = 210;
+let rollBack = 'откат';
+let fullPrice;
+let adaptive;
 
-let screens = "Простые, Сложные, Интерактивные"
-console.log(screens.length);   //Вывести в консоль длину строки из переменной screens
-//Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
-console.log(screens.toLowerCase().split())  
+alert ('My first lesson')
 
-let screenPrice = 24;
-//Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани”
-console.log("Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани")  //предложение
-
-let rollback  = 20;    
-
-let fullPrice = 50000;
-console.log(typeof fullPrice); //Вывести в консоль тип данных значений переменной fullPrice
-//Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
-console.log("Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани") //предложение
-
-let adaptive = true;     
-console.log(typeof adaptive);  //Вывести в консоль тип данных значений переменной adaptive
-
-//Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
-const result = "Процент отката посреднику за работу " + fullPrice *(rollback / 100)
-console.log(result);
-
-
+console.log('Hello my freid');
