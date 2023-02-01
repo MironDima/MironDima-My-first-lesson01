ba31e1822fd0223b531365444a82f801e2f774ff
«Dima_Mirontsov» <miron2098@mail.ru>
2023-02-01 11:59:34 +0300

    add

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
 "use strict";
 
 //блок обьявления переменных
-const title = getTitle(prompt('Как называется ваш проект?'));
+
+let title = getTitle(prompt('Как называется ваш проект?'));
 let screens =prompt("Какие типы экранов нужно разработать?",'Простые, Сложные, Интерактивные') 
 let screenPrice = +prompt('Сколько будет стоить данная работа?', 12000);
 let adaptive = confirm('Нужен ли адаптив на сайте?'); 
-const servicePrice1 = +prompt("Сколько это будет стоить?",200.10)
-const servicePrice2 = +prompt("Сколько это будет стоить?",200.50)
+
 let rollback  = 20;
+let allServicePrices
+let fullPrice
+let servicePercentPrice
+
+
+const servicePrice1 = +prompt("Сколько это будет стоить?",200.10)
+
+const servicePrice2 = +prompt("Сколько это будет стоить?",200.50)
+
 
 
 //блок описания функций
+const showTypeOf = function(variable) {
+	console.log(variable,typeof variable);
+}
+
+const  getAllServicePrices = function(){
+	return servicePrice1 + servicePrice2
+}
+allServicePrices = getAllServicePrices();
+
+function getFullPrice() {
+	return screenPrice + allServicePrices
+	}
+ fullPrice = getFullPrice()
+
+
+function getTitle(str) {
+	return  str[0].toUpperCase() + str.slice(1)
+}
+ getTitle(title);
+
+
+function getServicePercentPrices(){
+	return fullPrice - (fullPrice * (rollback / 100))  //9920.28
+}
+ servicePercentPrice = getServicePercentPrices()
 
 
 
 
 //Функционал
-
-const showTypeOf = function(variable) {
-		console.log(variable,typeof variable);
-}
-
 const getRollbackMessage = function(price) {
 if (price >= 30000) {
 	 return 'Даем скидку в 10%'
@@ -36,28 +65,6 @@ else if( price < 0){
 }
 }
 
-const  getAllServicePrices = function(price1,price2){
-	return price1 + price2
-}
-const allServicePrices = getAllServicePrices(servicePrice1,servicePrice2);
-
-function getFullPrice(sum,allSum) {
-	return sum + allSum
-	}
-
-const fullPrice = getFullPrice(screenPrice, allServicePrices)
-
-
-function getTitle(str) {
-	return str[0].toUpperCase() + str.slice(1)
-}
-getTitle(title)
-
-
-function getServicePercentPrices(){
-	return fullPrice - (fullPrice * (rollback / 100))  //9920.28
-}
-const servicePercentPrice = getServicePercentPrices()
 
 
 //блок вывода в консоль
@@ -66,11 +73,11 @@ showTypeOf(title)
 showTypeOf(screenPrice)
 showTypeOf(adaptive)
 
+
+
+
 console.log(screens);
 console.log(servicePrice1);
 console.log(servicePrice2);
 console.log(getRollbackMessage(fullPrice))
-
-
-
 
