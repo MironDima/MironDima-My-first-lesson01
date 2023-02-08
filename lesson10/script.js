e7d14368eab738c585b2780f7ee8d30226ca1047
«Dima_Mirontsov» <miron2098@mail.ru>
2023-02-08 20:36:25 +0300

    add 1,2 conditions

--- lesson10/script.js
+++ lesson10/script.js
@@ -1,2 +1,17 @@
 
-console.log();
+const book = document.querySelectorAll('.book')
+book[0].before(book[1])
+book[2].before(book[4])
+book[2].before(book[3])
+book[2].before(book[5])
+console.log(book);
+
+
+const url = document.querySelector('body')
+url.style.backgroundImage =  "url(./image/you-dont-know-js.jpg)"
+
+
+const bookThree = document.querySelectorAll('.book')
+ 
+
+console.log(book);
