7218d0a4f1048cf5f2a9b1523aca4d99b43ec28b
Miron Dima <mirontsov2098@mail.ru>
2023-03-26 10:44:13 +0300

    add lesson26

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hi');
+const sendData = (url,data) => {
+	return fetch(url, {
+		method: 'POST',
+		body: JSON.stringify(data),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	}).then(response => response.json())
+		.then(data => console.log(data))
+		.catch(error => console.log(error))
+}
+
+getData = () => {
+	const user = fetch('db.json')
+	user
+		.then(response => response.json())
+		.then(data => sendData('https://jsonplaceholder.typicode.com/posts', data))
+		.catch(error => console.log(error))
+}
+
+getData()
